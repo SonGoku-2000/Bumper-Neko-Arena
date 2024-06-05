fea378a4548edd6421eef08f1be042b27c4988f4
SonGoku-2000 <pablontxs@gmail.com>
2024-06-04 20:05:03 -0500

    Main: agregado player para pruebas

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include "bn_core.h"
 
+#include "bn_bg_palettes.h"
+#include "bn_colors.h"
+
+#include "bn_vector.h"
+#include "bna_hitbox.hpp"
+#include "bna_vector2.hpp"
+#include "bna_player.hpp"
+
+// limite sprites 128
+
 int main() {
     bn::core::init();
+    bn::bg_palettes::set_transparent_color(bn::colors::gray);
+    bn::vector<bna::Hitbox, 4> obstaculos;
+    obstaculos.push_back(bna::Hitbox(bna::Vector2(32, 0), bna::Vector2(18, 10), true));
+    bna::Player player;
+    player.spawn(obstaculos);
 
     while (true) {
+        player.update();
+
         bn::core::update();
     }
 }
