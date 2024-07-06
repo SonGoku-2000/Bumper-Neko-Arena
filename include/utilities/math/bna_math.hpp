40230222d98b7a346547fd89b2995ee5b509418a
SonGoku-2000 <pablontxs@gmail.com>
2024-07-05 20:04:34 -0500

    Math: agregado interpolacion

--- include/utilities/math/bna_math.hpp
+++ include/utilities/math/bna_math.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "bn_fixed.h"
+#include "bn_fixed_point.h"
 
 namespace bna {
     namespace math {
@@ -11,6 +12,20 @@ namespace bna {
                 return num + divisor;
             }
             return num;
+        }
+
+        [[nodiscard]] constexpr bn::fixed_point interpolate_x(const bn::fixed_point& p1, const bn::fixed_point& p2, const bn::fixed x) {
+            bn::fixed t = (x - p1.x()) / (p2.x() - p1.x());
+            bn::fixed y = p1.y() + t * (p2.y() - p1.y());
+
+            return bn::fixed_point(x, y);
+        }
+
+        [[nodiscard]] constexpr bn::fixed_point interpolate_y(const bn::fixed_point& p1, const bn::fixed_point& p2, const bn::fixed y) {
+            bn::fixed t = (y - p1.y()) / (p2.y() - p1.y());
+            bn::fixed x = p1.x() + t * (p2.x() - p1.x());
+
+            return bn::fixed_point(x, y);
         }
     } // namespace math
 } // namespace bna
