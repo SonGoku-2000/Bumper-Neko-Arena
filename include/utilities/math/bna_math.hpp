5cee1ba1832a05a18cda974c6c216c3c3ee96e3f
SonGoku-2000 <pablontxs@gmail.com>
2024-07-06 16:15:41 -0500

    Math: reducido division pos cero en interpolate

--- include/utilities/math/bna_math.hpp
+++ include/utilities/math/bna_math.hpp
@@ -2,6 +2,7 @@
 
 #include "bn_fixed.h"
 #include "bn_fixed_point.h"
+#include "bn_math.h"
 
 namespace bna {
     namespace math {
@@ -15,6 +16,10 @@ namespace bna {
         }
 
         [[nodiscard]] constexpr bn::fixed_point interpolate_x(const bn::fixed_point& p1, const bn::fixed_point& p2, const bn::fixed x) {
+            constexpr bn::fixed EPSILON(0.1);
+            if (bn::abs(p2.x() - p1.x()) < EPSILON) {
+                return  bn::fixed_point(x, p1.y());
+            }
             bn::fixed t = (x - p1.x()) / (p2.x() - p1.x());
             bn::fixed y = p1.y() + t * (p2.y() - p1.y());
 
@@ -22,6 +27,10 @@ namespace bna {
         }
 
         [[nodiscard]] constexpr bn::fixed_point interpolate_y(const bn::fixed_point& p1, const bn::fixed_point& p2, const bn::fixed y) {
+            constexpr bn::fixed EPSILON(0.1);
+            if (bn::abs(p2.y() - p1.y()) < EPSILON) {
+                return bn::fixed_point(p1.x(), y);
+            }
             bn::fixed t = (y - p1.y()) / (p2.y() - p1.y());
             bn::fixed x = p1.x() + t * (p2.x() - p1.x());
 
