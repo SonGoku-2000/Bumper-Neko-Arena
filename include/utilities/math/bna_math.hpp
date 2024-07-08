3a5a1f4ac052219e5f2dc925e7f054ccdd0991d7
SonGoku-2000 <pablontxs@gmail.com>
2024-07-07 20:24:40 -0500

    Math: lerp agregado

--- include/utilities/math/bna_math.hpp
+++ include/utilities/math/bna_math.hpp
@@ -15,6 +15,16 @@ namespace bna {
             return num;
         }
 
+
+        [[nodiscard]] constexpr bn::fixed lerp(bn::fixed number_1, bn::fixed number_2, bn::fixed t) {
+            return number_1 + t * (number_2 - number_1);
+        }
+
+        [[nodiscard]] constexpr bn::fixed_point lerp(const bn::fixed_point& point_1, const bn::fixed_point& point_2, const bn::fixed t) {
+            return bn::fixed_point(point_1.x() + t * (point_2.x() - point_1.x()), point_1.y() + t * (point_2.y() - point_1.y()));
+        }
+
+
         [[nodiscard]] constexpr bn::fixed_point interpolate_x(const bn::fixed_point& p1, const bn::fixed_point& p2, const bn::fixed x) {
             constexpr bn::fixed EPSILON(0.1);
             if (bn::abs(p2.x() - p1.x()) < EPSILON) {
