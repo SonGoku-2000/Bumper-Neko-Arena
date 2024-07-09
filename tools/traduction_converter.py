1fcaefe427b31b64bbaa4a13ccc3e0735c5a3595
SonGoku-2000 <pablontxs@gmail.com>
2024-07-09 13:57:00 -0500

    Traduction Converter: lanza error de caracter " " invalido

--- tools/traduction_converter.py
+++ tools/traduction_converter.py
@@ -2,18 +2,22 @@ from pathlib import Path
 import argparse
 from typing import Iterable
 import csv
+import sys
+import traceback
 
 
-def procesar_carpeta(folder_path: str, output_folder: str, recursive:bool):
+def procesar_carpeta(folder_path: str, output_folder: str, recursive: bool):
     for path in Path(folder_path).iterdir():
         if path.is_file():
             procesar_archivo(path.__str__(), output_folder)
 
         elif recursive and path.is_dir():
-            procesar_carpeta(path.__str__(),output_folder,recursive)
+            procesar_carpeta(path.__str__(), output_folder, recursive)
 
 
 def procesar_archivo(file_path: str, output_folder: str):
+    if (not comprobar_formato(file_path)):
+        return
     with open(file_path) as file:
         data = csv.reader(file, delimiter=";")
         idiomas: list
@@ -25,6 +29,23 @@ def procesar_archivo(file_path: str, output_folder: str):
         output_path.mkdir(exist_ok=True, parents=True)
         output_path = output_path.joinpath(Path(file_path).stem)
         crear_archivo(output_path.__str__(), idiomas, data)
+
+
+def comprobar_formato(file_path: str) -> bool:
+    path = Path(file_path)
+    if file_path.find(" ") != -1:
+        try:
+            raise ValueError(
+                f"\nError: Invalid file name: '{path.name}' (invalid character: ' ')")
+        except ValueError as ex:
+            sys.stderr.write(str(ex) + '\n\n')
+            traceback.print_exc()
+            raise
+
+    if (path.suffix == ".csv" or path.suffix == ".CSV"):
+        return True
+
+    return False
 
 
 def crear_archivo(path: str, idiomas: list, filas: Iterable | list[list[str]]):
@@ -124,10 +145,10 @@ if __name__ == "__main__":
     parser.add_argument('--recursive', "-r", required=False, default=True,
                         type=bool, help='If a folder is given in dirs, it processes it recursively True by default.')
 
-    args = parser.parse_args()
-    # args = parser.parse_args([
-    #     '-o', 'external_tool',
-    #     '-d', 'traduction', 'traduccion_prueba copy.csv'
-    # ])
+    # args = parser.parse_args()
+    args = parser.parse_args([
+        '-o', 'external_tool',
+        '-d', 'traduction', 'traduccion_prueba copy.csv'
+    ])
 
     process(args.output, args.dirs, args.recursive)
