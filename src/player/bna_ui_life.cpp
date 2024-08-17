71dcbb4c79352d43139344abc88e52e3646c0238
SonGoku-2000 <pablontxs@gmail.com>
2024-08-17 14:40:55 -0500

    UI Life: barra de vida baja correctemente

--- src/player/bna_ui_life.cpp
+++ src/player/bna_ui_life.cpp
@@ -29,21 +29,27 @@ void bna::UILife::setCar(bna::Car& car) {
 }
 
 void bna::UILife::update() {
-    // if (_car->getLife() > (bna::limit_values::MAX_LIFE / 4) * 3) {
-    //     _sprite.set_item(bn::sprite_items::life_bar, 0);
-    // }
-    // else  if (_car->getLife() > (bna::limit_values::MAX_LIFE / 4) * 2) {
-    //     _sprite.set_item(bn::sprite_items::life_bar, 1);
-    // }
-    // else  if (_car->getLife() > bna::limit_values::MAX_LIFE / 4) {
-    //     _sprite.set_item(bn::sprite_items::life_bar, 2);
-    // }
-    // else if (_car->getLife() > 0) {
-    //     _sprite.set_item(bn::sprite_items::life_bar, 3);
-    // }
-    // else {
-    //     _sprite.set_item(bn::sprite_items::life_bar, 4);
-    // }
+    if (_car->getLife() > (bna::limit_values::MAX_LIFE / 7) * 6) {
+        _healthBar.set_item(bn::sprite_items::health_bar, 0);
+    }
+    else if (_car->getLife() > (bna::limit_values::MAX_LIFE / 7) * 5) {
+        _healthBar.set_item(bn::sprite_items::health_bar, 1);
+    }
+    else if (_car->getLife() > (bna::limit_values::MAX_LIFE / 7) * 4) {
+        _healthBar.set_item(bn::sprite_items::health_bar, 2);
+    }
+    else if (_car->getLife() > (bna::limit_values::MAX_LIFE / 7) * 3) {
+        _healthBar.set_item(bn::sprite_items::health_bar, 3);
+    }
+    else if (_car->getLife() > (bna::limit_values::MAX_LIFE / 7) * 2) {
+        _healthBar.set_item(bn::sprite_items::health_bar, 4);
+    }
+    else if (_car->getLife() > (bna::limit_values::MAX_LIFE / 7)) {
+        _healthBar.set_item(bn::sprite_items::health_bar, 5);
+    }
+    else {
+        _healthBar.set_item(bn::sprite_items::health_bar, 6);
+    }
 }
 
 void bna::UILife::_setSprite() {
